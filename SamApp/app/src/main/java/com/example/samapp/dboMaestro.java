1adeff96ddee1a45253701306dc922381e4bc45e
Jose Garcia <drokemx@gmail.com>
2019-05-12 09:44:38 -0700

    model

---
+++ SamApp/app/src/main/java/com/example/samapp/dboMaestro.java
@@ -0,0 +1,54 @@
+package com.example.samapp;
+
+public class dboMaestro {
+
+    private String Uid;
+    private String nombre;
+    private String correo;
+    private long telefono;
+
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    /*
+
+    public String toString() {
+        return "Name: " + this.nombre + "\nCorreo:" + this.correo + "\nTelefono:"+ this.telefono;
+    }
+    */
+
+
+    public String toString() {
+        return this.nombre;
+    }
+}
